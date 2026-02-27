0ce8ed0b535367a433b2500ec4b2db3f7b777fc7
fatoungom21 <fatoungom212004@gmail.com>
2026-02-27 14:51:57 +0100

    Update CV.py

--- CV.py
+++ CV.py
@@ -1,46 +1,41 @@
 import streamlit as st
+
 st.header("CURRICULUM VITAE")
- st.image("ndeya.jpg")
- st.write("Fatou Ngom")
- st.sidebar.write("CONTACTS")
- st.sidebar.write("Email: ngomfatou21102004@gmail.com")
- st.sidebar.write("Tel: 776857741")
- st.sidebar.write("Adresse: Keur Mbaye Fall,Toll Diaz")
+st.sidebar.write("FATOU NGOM")
+st.sidebar.write("CONTACTS")
+st.sidebar.write("Email: ngomfatou21102004@gmail.com")
+st.sidebar.write("Tel: 776857741")
+st.sidebar.write("Adresse: Keur Mbaye Fall,Toll Diaz")
+
+    
+st.subheader("FORMATIONS")
+st.write("* BAC L2:2022-2023")
+st.write("*Licence en Geographie:2023-2026")
+st.write("*BTS en Geomatique:2024-2026")
+   
  
- st.subheader("Projets académiques")
- st.write("Formations")
- st.write("2022-2023: BAC L2")
- st.write("2023-2024: Licence1 en Geographie")
- st.write("2024-2025: Licence2 en Geographie,Licence1 en Geomatique")
- st.write("2025-2026: Licence3 en Geographie")
- st.write("2025-2026:BTS en Geomatique")
-
-
-
-st.subheader("DESCRIPTION")
-st.write("Etudiante passionnée par l'analyse spatiale,la cartographie et la geographie ,je me spécialise dans l'acquisition et le traitement de données spatiales rigoureuse et polyvalente ")
-
 
 st.subheader("Logiciel Maitriser")
-st.write("ARCGIS") 
-st.write("QGIS")
-st.write("Agisoft Metashape")
-st.write("Pix4D Capture")
-st.write("PowerPoint")
-st.write("Erdas")
-st.write("MicoStation")
-st.write("Word")
-st.write("Excel")
+st.write("*ARCGIS")
+st.write("*ARCGIS")
+st.write("*QGIS")     
+st.write("*Agisoft Metashape") 
+st.write("*Pix4D Capture") 
+st.write("*Word") 
+st.write("*Excel") 
+st.write("*AutoCard")   
+st.write("*Looping")  
 
 
 st.subheader("Compétences")
-st.write("Conception de base de données")
-st.write("SIG")
-st.write(" Potogrammetrie:Traitement d'image")
-st.write("Topographie")
-st.write("Technique de base de la Géographie: réalisation de cartes ( themeatiques et de situation)")
-st.write("Maitrise des techniques de collecte données:application Mobile Topo ,Qfield")
-st.write("Maitrise avancée du Pack Office (EXCEL pour le traitement de données topographiques, WORD pour la réedaction de rapports technique, POWER POINT pour la Presentation de projets")
+st.write("*Conception de base de données")
+st.write("*SIG")
+st.write("*Potogrammetrie:Traitement d'image")
+st.write("*Topographie")
+st.write("*Technique de base de la Géographie: réalisation de cartes ( themeatiques et de situation)")
+st.write("*Maitrise des techniques de collecte données:application Mobile Topo ,Qfield")
+st.write("*Maitrise avancée du Pack Office (EXCEL pour le traitement de données topographiques, WORD pour la réedaction de rapports technique, POWER POINT pour la Presentation de projets)")
+   
 
 st.subheader("Perspectives")
 st.write("j'aspire à mettre mes compétences en Sig et téledetection au service de projets de développement territoial")
