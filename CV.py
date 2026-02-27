5219fec07997e372de74ada8b0c927750ac2d0b5
fatoungom21 <fatoungom212004@gmail.com>
2026-02-27 16:01:17 +0100

    Update CV.py

--- CV.py
+++ CV.py
@@ -7,34 +7,39 @@ st.sidebar.write("Email: ngomfatou21102004@gmail.com")
 st.sidebar.write("Tel: 776857741")
 st.sidebar.write("Adresse: Keur Mbaye Fall,Toll Diaz")
 
+st.divider()
     
-st.subheader("FORMATIONS")
-st.write("* BAC L2:2022-2023")
-st.write("*Licence en Geographie:2023-2026")
-st.write("*BTS en Geomatique:2024-2026")
+st.subheader("##FORMATIONS")
+st.markdown("""
+* BAC L2:2022-2023
+*Licence en Geographie:2023-2026
+*BTS en Geomatique:2024-2026
    
  
 
 st.subheader("Logiciel Maitriser")
-st.write("*ARCGIS")
-st.write("*ARCGIS")
-st.write("*QGIS")     
-st.write("*Agisoft Metashape") 
-st.write("*Pix4D Capture") 
-st.write("*Word") 
-st.write("*Excel") 
-st.write("*AutoCard")   
-st.write("*Looping")  
-
+st.markdown("""
+*ARCGIS
+*ARCGIS
+*QGIS    
+*Agisoft Metashape
+*Pix4D Capture
+*Word
+*Excel 
+*AutoCard
+*Looping  
+""")
 
 st.subheader("Compétences")
-st.write("*Conception de base de données")
-st.write("*SIG")
-st.write("*Potogrammetrie:Traitement d'image")
-st.write("*Topographie")
-st.write("*Technique de base de la Géographie: réalisation de cartes ( themeatiques et de situation)")
-st.write("*Maitrise des techniques de collecte données:application Mobile Topo ,Qfield")
-st.write("*Maitrise avancée du Pack Office (EXCEL pour le traitement de données topographiques, WORD pour la rédaction de rapports technique, POWER POINT pour la Presentation de projets)")
+st.markdown("""
+*Conception de base de données
+*SIG
+*Potogrammetrie:Traitement d'image
+*Topographie
+*Technique de base de la Géographie: réalisation de cartes ( themeatiques et de situation)
+*Maitrise des techniques de collecte données:application Mobile Topo ,Qfield
+*Maitrise avancée du Pack Office (EXCEL pour le traitement de données topographiques, WORD pour la rédaction de rapports technique, POWER POINT pour la Presentation de projets)
+""")
    
 
 st.subheader("Perspectives")
